3ae5048563b1cdb782753a4c2a325b5a57509718
Khanh <Khanh13Nguyen13@gmail.com>
2024-11-13 11:19:10 +0100

    list of cities

--- src/components/ListOfCities/ListOfCities.js
+++ src/components/ListOfCities/ListOfCities.js
@@ -13,23 +13,34 @@ const cities = [
 
 export default function ListOfCities() {
 	const [activeStatus, setActiveStatus] = useState(false);
-	const activeBtn = classNames({
-		"list-item": true,
-		"is-active": activeStatus,
-	});
+	const handleClick = (e) => {
+		setActiveStatus(!activeStatus);
+		console.log(e.target);
+	};
+
+	// const activeBtn = classNames({
+	// 	"list-item": true,
+	// 	"is-active": activeStatus,
+	// });
 
 	return (
 		<ul className="list">
 			{cities.map((city) => (
 				<li className="list-item">
 					<button
+						className={`list-btn ${activeStatus ? "is-active" : ""}`}
+						onClick={handleClick}
+					>
+						{city}
+					</button>
+					{/* <button
 						className={activeBtn}
 						onClick={(e) => {
 							setActiveStatus(!activeStatus);
 						}}
 					>
 						{city}
-					</button>
+					</button> */}
 				</li>
 			))}
 		</ul>
