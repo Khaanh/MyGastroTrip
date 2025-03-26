cbf3649cd3aec7d9416fb7375fbd570a3564f6b7
Khanh <Khanh13Nguyen13@gmail.com>
2025-03-26 23:57:51 +0100

    Styled menu

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,19 +1,20 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
+import styles from "./PageNav.module.css";
 
 export default function PageNav() {
 	return (
-		<nav>
+		<nav className={styles.nav}>
 			<Logo />
-			<ul>
-				<li>
+
+			<ul className={styles.menuList}>
+				<li className={styles.menuItems}>
 					<NavLink to="/">Home</NavLink>
 				</li>
-				<li>
+				<li className={styles.menuItems}>
 					<NavLink to="/product">Product</NavLink>
 				</li>
-				<li>
+				<li className={styles.menuItems}>
 					<NavLink to="/login">Login</NavLink>
 				</li>
 			</ul>
