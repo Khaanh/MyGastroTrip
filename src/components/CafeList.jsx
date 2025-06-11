653263f132cc3e80d016b75c669d235f2bee6694
Khaanh <khanh13nguyen13@gmail.com>
2025-06-11 23:40:34 +0200

    feat: improving reusability with props

--- src/components/CafeList.jsx
+++ src/components/CafeList.jsx
@@ -73,15 +73,6 @@ const cafeList = [
 export default function CafeList({ selectedCity }) {
 	return (
 		<div>
-			<TextExpander
-				content={
-					"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus animi totam excepturi laudantium? Rem ipsa maiores alias provident dolore ut voluptates, consectetur praesentiu Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus animi totam excepturi laudantium? Rem ipsa maiores alias provident dolore ut voluptates, Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus animi totam excepturi laudantium? Rem ipsa maiores alias provident dolore ut voluptates, consectetur praesentiu Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus animi totam excepturi laudantium? Rem ipsa maiores alias provident dolore ut voluptates"
-				}
-				expandButtonText={"Read more"}
-				collapseButtonTex={"Show less"}
-				symbols={50}
-				// btnDisabled={false}
-			/>
 			<ul className={styles.List}>
 				{/* Title
 					Rating
