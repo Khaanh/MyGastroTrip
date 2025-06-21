29fd0370aef2455f0df206cea29426b9f19a03c0
Khaanh <khanh13nguyen13@gmail.com>
2025-06-21 17:21:27 +0200

    feat: add structure to cafe details component

--- src/components/CafeDetails.jsx
+++ src/components/CafeDetails.jsx
@@ -3,5 +3,26 @@ import styles from "./CafeDetails.module.css";
 
 export default function CafeDetails() {
 	const { id } = useParams();
-	return <h1>CafeDetails {id}</h1>;
+	return (
+		<main>
+			<h1>Name {id}</h1>
+			<div>
+				<p>Rating</p>
+				<ul>
+					<li>Design</li>
+					<li>Services</li>
+					<li>Food</li>
+					<li>Would You go back here?</li>
+				</ul>
+			</div>
+			<div>
+				<h2>Contacts:</h2>
+				<ul>
+					<li>Phone</li>
+					<li>Address</li>
+					<li>Working hours</li>
+				</ul>
+			</div>
+		</main>
+	);
 }
